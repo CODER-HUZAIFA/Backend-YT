ce6b23e13e7de4ccbfe80123096d6bb67b3930f5
CODER-HUZAIFA <ansarihuzaifa9825@gmail.com>
2024-04-19 19:30:13 +0530

    EJS and cookies

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 import express from "express"
-
+import path from "path"
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static("C:\Users\HUZAIFA ANSARI\Documents\Backend Project\public"))
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.render("index")
+});
 
 export { app }
