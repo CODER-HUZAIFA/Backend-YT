4273fcbf45dc4cafb320ac71c6ce281d6751151d
CODER-HUZAIFA <ansarihuzaifa9825@gmail.com>
2024-08-28 14:07:59 +0530

    Path m

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser"
 import session from "express-session"
 import userRoutes from "./routes/user.router.js"
 import staticRouter from './routes/staticRoutes.js'
+import * as path from 'path'
 import { profileCheck, userData } from "./middleware/user.middleware.js"
 const app = express()
 
@@ -23,7 +24,7 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(express.static("C:/Users/HUZAIFA ANSARI/Documents/Backend Project/public"))
+app.use(express.static(path.join(__dirname, 'public')))
 app.set("view engine", "ejs")
 
 app.use(session({
