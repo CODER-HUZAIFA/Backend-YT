987d2b9ed6a655b9be0c2824490a97f682ce8505
CODER-HUZAIFA <ansarihuzaifa9825@gmail.com>
2024-08-28 13:54:49 +0530

    To deploy

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import staticRouter from './routes/staticRoutes.js'
 import { profileCheck, userData } from "./middleware/user.middleware.js"
 const app = express()
 
+import connectDB from "./db/index.js"
+import 'dotenv/config'
+
+
+connectDB()
+.then(() => {
+    app.listen(3000, () => {
+        console.log("App is listening on port 3000")
+    })
+})
+.catch((error) => {
+    console.log("Error in DB Connection")
+})
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
