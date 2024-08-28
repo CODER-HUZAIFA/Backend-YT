1ed92f29642c35bbaa7318e2214e36b3c747a1b6
CODER-HUZAIFA <ansarihuzaifa9825@gmail.com>
2024-08-28 14:18:28 +0530

    Final Commit to deploy

--- src/app.js
+++ src/app.js
@@ -29,9 +29,9 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 app.set("view engine", "ejs")
-
+console.log(__dirname)
 app.use(session({
     resave: false,
     saveUninitialized: false,
